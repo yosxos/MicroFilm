f97cc6b20a6befc951c768c257de9a457afb04c4
yasait <yassirx2012@gmail.com>
2023-03-01 17:30:12 +0100

    auth back end update

--- BackEnd/auth/app/main.py
+++ BackEnd/auth/app/main.py
@@ -20,7 +20,13 @@ app.add_middleware(
 class User(BaseModel):
     username: str
     password: str
-    
+
+class UserInDb(BaseModel):
+    id: int
+    username: str
+    password:str
+    token: str
+
 class Settings(BaseModel):
     authjwt_secret_key: str = "my_jwt_secret"
 @AuthJWT.load_config
@@ -32,22 +38,17 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
         status_code=exc.status_code,
         content={"detail": exc.message}
     )
-@app.get("/")
-def read_root():
-    return {"Hello": "World Rasyue"}
+
 @app.post('/login')
 def login(user: User, Authorize: AuthJWT = Depends()):
     #user.username
     #user.password
     # this is the part where we will check the user credentials with our database record
-    #but since we are not going to use any db, straight away we will just create the token and send it back
     # subject identifier for who this token is for example id or username from database
     access_token = Authorize.create_access_token(subject=user.username)
     return {"access_token": access_token}
 @app.get('/test-jwt')
 def user(Authorize: AuthJWT = Depends()):
-    
     Authorize.jwt_required()
-    return {"user": 123124124, 'data': 'jwt test works'} 
-    #current_user = Authorize.get_jwt_subject()
-    #return {"user": current_user, 'data': 'jwt test works'}    
+    current_user = Authorize.get_jwt_subject()
+    return {"user": current_user, 'data': 'jwt test works'}    
