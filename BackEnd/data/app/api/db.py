00e15039cd64c568fc1cf9048ac1645d85585819
cillian51 <cillian.rosay@gmail.com>
2023-03-01 17:33:15 +0100

    [ADD] db

---
+++ BackEnd/data/app/api/db.py
@@ -0,0 +1,32 @@
+
+from sqlalchemy import (Column, Integer, MetaData, String, Table, Float, create_engine, ARRAY)
+
+from databases import Database
+
+DATABASE_URL = 'postgresql://cillian:root@localhost/movie_app_db'
+
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+movies = Table(
+    'movies',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('overview', String(250)),
+    Column('runtime',Integer),
+    Column('note_average',Float),
+)
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50)),
+    Column('email', String(250)),
+    Column('token', String(1024)),
+    Column('password', String(1024))
+)
+
+
+database = Database(DATABASE_URL)
